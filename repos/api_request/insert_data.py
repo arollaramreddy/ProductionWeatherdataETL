178d5b1356829e220ca0abf8314dd2ed9c698ffa
arollaramreddy <arollaramreddy@gmail.com>
2026-05-12 15:15:55 -0700

    connect_database() function in repos/api_request/insert_data.py working and connection establishing succesfully

--- repos/api_request/insert_data.py
+++ repos/api_request/insert_data.py
@@ -1,3 +1,22 @@
 from api_requests import mock_fetch_data
+import psycopg2
 
-print(mock_fetch_data())
+#function for connection to postgres database
+def connect_database():
+    try:
+        print("connecting to database started......")
+        conn=psycopg2.connect(
+            host="localhost",
+            port=5001,
+            dbname="weather_db",
+            user="arollaramreddy",
+            password="weather_db_password"
+        )
+        
+        return conn
+    
+    except psycopg2.Error as e:
+        print(f"Database connection failed: {e}")
+        
+connect_database()
+        
